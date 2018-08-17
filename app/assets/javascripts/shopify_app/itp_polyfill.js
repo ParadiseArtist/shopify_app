51b4d24b26416e62e46203af263591662ed80cae
Francine Navarro <francine.navarro@shopify.com>
2018-08-17 15:38:57 -0400

    Remove unused shouldTriggerCookiePartitioning function

--- app/assets/javascripts/shopify_app/itp_polyfill.js
+++ app/assets/javascripts/shopify_app/itp_polyfill.js
@@ -1,12 +1,3 @@
-function shouldTriggerCookiePartitioning() {
-  var userAgent =  window.navigator.userAgent;
-  var isSafari = userAgent.indexOf('Safari');
-  var versionNumber = isSafari ? parseFloat(userAgent.match(/Version\/(\d+\.?\d*)/)[1]) : null;
-  
-  // TODO: Replace with library for checking user-agents
-  return document.hasStorageAccess && isSafari && versionNumber >= 12;
-}
-
 function setCookieAndRedirect() {
   document.cookie = "shopify.cookies_persist=true";
   window.location.href = window.shopOrigin + "/admin/apps/" + window.apiKey;
